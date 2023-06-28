056ea7dda47d8188e7827cbc61d0d5a5a17287d9
Harkeerat Kanwal <hsk532991@gmail.com>
2023-06-28 15:10:14 -0400

    Create an introduction

---
+++ src/components/Title.tsx
@@ -0,0 +1,16 @@
+import Styles from "../../styles/components/Title.module.css";
+
+export interface TitleProps {}
+
+export function Title(props: TitleProps) {
+  return (
+    <div className={Styles["title-container"]}>
+      <div className={Styles["name-container"]}>
+        <span className={Styles.firstname}>Harkeerat</span>
+        &nbsp;
+        <span>Kanwal</span>
+      </div>
+      <span className={Styles.subtitle}>A Full-Stack Developer</span>
+    </div>
+  );
+}
